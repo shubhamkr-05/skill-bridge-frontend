76d725585a563de042571eb5b49a3d6bd4684ea8
shubhamkr-05 <shubhamkragarwal0511@gmail.com>
2025-08-07 16:57:43 +0530

    backendURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:"http://localhost:8000", // replace with your backend URL
+  //baseURL:"http://localhost:8000", // replace with your backend URL
+  baseURL: process.env.REACT_APP_API_URL, 
   withCredentials: true, // only if you're using cookies
 });
 
