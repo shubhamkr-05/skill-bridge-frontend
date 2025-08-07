6bf9067d2d2b8bba2a2f55e14221f8dac58efa02
shubhamkr-05 <shubhamkragarwal0511@gmail.com>
2025-08-07 19:00:47 +0530

    test

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+console.log('process.env.REACT_APP_API_URL', process.env.REACT_APP_API_URL)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
