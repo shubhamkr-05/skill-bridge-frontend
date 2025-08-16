d06de2fe7970218c9a30934d8506cc73bc8fded4
shubhamkr-05 <shubhamkragarwal0511@gmail.com>
2025-08-16 14:52:36 +0530

    .

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,5 @@
 import { io } from "socket.io-client";
 
 const socket = io("https://nidaan-6jyx.onrender.com"); // your backend URL
+//const socket = io("http://localhost:8000");
 export default socket;
